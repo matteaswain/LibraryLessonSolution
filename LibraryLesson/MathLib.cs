20e6daa8a9c15ac7bd53c498f65be11fb1154633
Mattea Swain <mmswain@tql.com>
2021-05-25 12:12:32 -0400

    Add project files.

---
+++ LibraryLesson/MathLib.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LibraryLesson
+{
+    public class MathLib
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b; 
+        }
+
+        public static int Sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Mult(int a, int b)
+        {
+            return a * b;
+        }
+        public static int Division(int a, int b)
+        {
+            return a / b;
+        }
+
+        public static int Modulo(int a, int b)
+        {
+            return ((a / b) * b) - a;
+                     
+        }
+
+        
+    }
+}
---
+++ TestLibrary/Program.cs
@@ -0,0 +1,16 @@
+﻿using LibraryLesson;
+using System;
+
+namespace TestLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var OnePlusTwo = MathLib.Add(1, 2);
+
+            var ans = MathLib.Modulo(2, 2);
+
+        }
+    }
+}
